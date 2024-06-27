74bff3a58b84a4f9a796aa5e92c394b7bba67082
Maia <vincecake008@gmail.com>
2024-06-27 09:15:19 -0300

    feat: segunda versão do código

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function calcularPotencia(base, expoente) {
+/*function calcularPotencia(base, expoente) {
     if (expoente === 0) {
       return 1;
     } else if (expoente === 1) {
@@ -15,4 +15,25 @@ function calcularPotencia(base, expoente) {
   const resultado = calcularPotencia(base, expoente);
   
   console.log(`${base} elevado à potência ${expoente} é igual a ${resultado}`);
+  */
+
+const prompt = require(`prompt-sync`)();
+
+console.log("Vamos calcular uma potência");
+
+while (true) {
+
+  let base = +prompt("Base: ");
+  base = +base;
+  let expoente = +prompt("Expoente: ");
+  let resultado = base;
+  let base_aux= base;
   
+  for (let j = 1; j < expoente; j++) {
+    for (let i = 0; i < base; i++) {
+      resultado += base;
+    }
+    base_aux = resultado
+  }
+  console.log(resultado);
+}
